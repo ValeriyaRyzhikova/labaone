43ac2c0d7f53fa0ee1073295e394e97dfe4c4966
ValeriyaRyzhikova <rvaleriav@yandex.ru>
2020-04-10 00:32:32 +0700

    Добавлен файловый менеджер.

---
+++ fmanager.cpp
@@ -0,0 +1,52 @@
+#include "fmanager.h"
+
+FManager::FManager()
+{
+    timer = new QTimer(this);
+    connect(timer, SIGNAL(timeout()), this, SLOT(updateTime()));
+    timer->start(1000);
+}
+
+void FManager::addFile(QString name)
+{
+    fileList.append(new QFile(name));
+    state.append(QFile(name).size());
+}
+
+void FManager::delFile(QString name)
+{
+    int index_del=this->findFile(name);
+    if (index_del==-1) return;
+    fileList.removeAt(index_del);
+    state.removeAt(index_del);
+}
+
+int FManager::findFile(QString name)
+{
+    for(int i=0; i<fileList.size(); i++)
+        if(fileList[i]->fileName()==name)
+            return i;
+    return -1;
+}
+
+void FManager::updateTime()
+{
+    for(int i=0; i<fileList.size(); i++)
+       if ((fileList[i]->size())!=state[i])
+       {
+           QString name = fileList[i]->fileName();
+           state[i] = fileList[i]->size();
+           emit alterWatcher(name);
+       }
+
+}
+
+FManager *FManager::instance = 0;
+
+void FManager::addList(QList<QString> name_list)
+{
+    for(QString name: name_list)
+    {
+        this->addFile(name);
+    }
+}
---
+++ fmanager.h
@@ -0,0 +1,36 @@
+#ifndef FMANAGER_H
+#define FMANAGER_H
+#include <QList>
+#include <QFile>
+#include <QString>
+#include <QTimer>
+#include <QTime>
+#include<iostream>
+using namespace std;
+
+class FManager:public QObject
+{
+    Q_OBJECT
+    static FManager* instance;
+    QList<QFile*> fileList;
+    QList<int> state;
+    QTimer* timer;
+    FManager();
+public:
+    static FManager* getInstance(){
+        if (!instance)
+            instance=new FManager;
+        return instance;
+    }
+    void addList(QList<QString>);
+    void addFile(QString);
+    void delFile(QString);
+    int findFile(QString);
+
+public slots:
+    void updateTime();
+signals:
+    void alterWatcher(QString name);
+};
+
+#endif // FMANAGER_H
